e568a5f94b8d4521a0c97980db31bc601654b47b
윤형준 <shawnyoon@yunhyeongjuns-MacBook-Pro.local>
2022-12-20 09:24:28 +0900

    불필요한 주석 제거

--- src/utils/loginRequired.js
+++ src/utils/loginRequired.js
@@ -2,19 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const verifyUser = async (req, res, next) => {
   try {
-    // console.log(req.headers)
-    // const { jwtoken } = req.headers.authorization;
     const jwtoken = req.headers.authorization;
-
-    // await jwt.verify(
-    //   jwtoken,
-    //   process.env.SECRETE_KEY,
-    //   (err, decoded_payload) => {
-    //     if (decoded_payload != undefined) {
-    //       req.decodedJwtPayload = decoded_payload;
-    //     }
-    //   }
-    // );
 
     if (jwtoken != undefined) {
       const decoded = await jwt.verify(jwtoken, process.env.SECRETE_KEY);
